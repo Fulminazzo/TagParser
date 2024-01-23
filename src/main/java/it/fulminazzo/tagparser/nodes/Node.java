e9aae6eca2e0665e55aeec2c6a8b2735f6e42c10
Fulminazzo <fulminazzo21@gmail.com>
2024-01-23 22:43:57 +0100

    Added various serializing methods

--- src/main/java/it/fulminazzo/tagparser/nodes/Node.java
+++ src/main/java/it/fulminazzo/tagparser/nodes/Node.java
@@ -1,88 +1,167 @@
 package it.fulminazzo.tagparser.nodes;
 
+import it.fulminazzo.tagparser.nodes.exceptions.EndOfStreamException;
+import it.fulminazzo.tagparser.nodes.exceptions.NodeException;
+import it.fulminazzo.tagparser.nodes.exceptions.NotValidTagNameException;
+import it.fulminazzo.tagparser.nodes.exceptions.files.FileDoesNotExistException;
+import it.fulminazzo.tagparser.nodes.exceptions.files.FileIsDirectoryException;
 import it.fulminazzo.tagparser.utils.StringUtils;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@Getter
+@Setter
 public class Node {
+    static final String TAG_NAME_REGEX = "[A-Za-z][A-Za-z0-9_-]*[A-Za-z0-9]";
     protected final String tagName;
     protected final Map<String, String> attributes;
-    protected boolean closed;
     protected Node next;
-    protected Node child;
-    protected String contents;
 
-    //TODO: Checks for tag name!
     Node(String tagName) {
-        this.tagName = tagName.endsWith("/") ? tagName.substring(0, tagName.length() - 1) : tagName;
+        if (!tagName.matches(TAG_NAME_REGEX))
+            throw new NotValidTagNameException(tagName);
+        this.tagName = tagName;
         this.attributes = new LinkedHashMap<>();
-        this.closed = !tagName.endsWith("/");
     }
 
-    public String getTagName() {
-        return tagName;
+    public void setAttribute(String key, String value) {
+        this.attributes.put(key, StringUtils.removeQuotes(value));
+    }
+
+    public String getAttribute(String key) {
+        return this.attributes.get(key);
     }
 
     public void setAttributes(Map<String, String> attributes) {
         this.attributes.clear();
-        if (attributes != null) attributes.forEach((k, v) ->
-                this.attributes.put(k, StringUtils.removeQuotes(v)));
+        if (attributes != null) attributes.forEach(this::setAttribute);
     }
 
-    public Map<String, String> getAttributes() {
-        return attributes;
+    public void setNext(String string) {
+        setNext(Node.newNode(string));
     }
 
-    void setClosed(boolean closed) {
-        this.closed = closed;
+    public void setNext(File file) {
+        setNext(Node.newNode(file));
     }
 
-    public boolean isClosed() {
-        return closed;
+    public void setNext(InputStream stream) {
+        setNext(Node.newNode(stream));
     }
 
     public void setNext(Node next) {
         this.next = next;
     }
 
-    public Node getNext() {
-        return next;
+    public String toHTML() {
+        final StringBuilder builder = new StringBuilder("<").append(tagName);
+        if (!this.attributes.isEmpty()) {
+            this.attributes.forEach((k, v) -> {
+                if (k == null) return;
+                builder.append(" ").append(k);
+                if (v != null) builder.append("=\"")
+                        .append(v.replace("\"", "\\\""))
+                        .append("\"");
+            });
+        }
+        return builder.append("/>").toString();
     }
 
-    public void setChild(Node child) {
-        this.child = child;
+    public String toJson() {
+        final StringBuilder builder = new StringBuilder("{");
+
+        Class<?> clazz = this.getClass();
+        while (clazz != Object.class) {
+            for (Field field : clazz.getDeclaredFields())
+                if (!Modifier.isStatic(field.getModifiers())) {
+                    if (builder.length() != 1) builder.append(",");
+                    builder.append("\"").append(field.getName()).append("\":");
+                    try {
+                        field.setAccessible(true);
+                        Object value = field.get(this);
+                        if (value instanceof Map) {
+                            builder.append("{");
+                            Map<?, ?> obj = (Map<?, ?>) value;
+                            obj.forEach((k, v) -> {
+                                if (builder.charAt(builder.length() - 1) != '{') builder.append(",");
+                                if (k == null) return;
+                                builder.append("\"").append(k).append("\":");
+                                if (v != null) builder.append("\"")
+                                        .append(v.toString().replace("\"", "\\\""))
+                                        .append("\"");
+                                else builder.append("null");
+                            });
+                            builder.append("}");
+                        } else {
+                            if (value instanceof String) builder.append("\"");
+                            if (value instanceof Node) builder.append(((Node) value).toJson());
+                            else builder.append(value);
+                            if (value instanceof String) builder.append("\"");
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            clazz = clazz.getSuperclass();
+        }
+        return builder.append("}").toString();
     }
 
-    public Node getChild() {
-        return child;
-    }
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder(this.getClass().getSimpleName() + " {");
 
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
-    public String getContents() {
-        return contents;
+        Class<?> clazz = this.getClass();
+        while (clazz != Object.class) {
+            for (Field field : clazz.getDeclaredFields())
+                if (!Modifier.isStatic(field.getModifiers())) {
+                    builder.append("\n    ").append(field.getName()).append(": ");
+                    try {
+                        field.setAccessible(true);
+                        Object value = field.get(this);
+                        if (value instanceof String) builder.append("\"");
+                        if (field.getName().equals("next") && next != null)
+                            builder.append(next.getClass().getSimpleName())
+                                    .append("(\"")
+                                    .append(next.getTagName())
+                                    .append("\")");
+                        else builder.append(value);
+                        if (value instanceof String) builder.append("\"");
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            clazz = clazz.getSuperclass();
+        }
+        return builder.append("\n}").toString();
     }
 
     public static Node newNode(String string) {
         return string == null ? null : newNode(new ByteArrayInputStream(string.getBytes()));
     }
 
-    public static Node newNode(InputStream stream) {
-        return Node.newNode(new StringBuilder(), stream);
+    public static Node newNode(File file) {
+        try {
+            if (file.isDirectory()) throw new FileIsDirectoryException(file);
+            return newNode(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            throw new FileDoesNotExistException(file);
+        }
     }
 
-    private static Node newNode(final StringBuilder buffer, InputStream stream) {
+    public static Node newNode(InputStream stream) {
         try {
+            final StringBuilder buffer = new StringBuilder();
             final Map<String, String> attributes = new LinkedHashMap<>();
 
             int read = stream.read();
-            if (read == -1) throw new IOException("End of stream");
+            if (read == -1) throw new EndOfStreamException();
 
             // Read tag name from given stream.
             if (read != '>') {
@@ -92,7 +171,11 @@ public class Node {
                     else buffer.append((char) read);
             }
             final String tagName = buffer.toString();
-            final Node node = new Node(tagName);
+            final Node node;
+            if (tagName.endsWith("/"))
+                node = new Node(tagName.substring(0, tagName.length() - 1));
+//            else node = new ContainerNode(tagName);
+            else node = null;
             buffer.setLength(0);
 
             if (read == ' ') {
@@ -120,67 +203,35 @@ public class Node {
                 buffer.setLength(0);
             }
 
-            // Read contents from given stream.
-            final String end = "</" + tagName + ">";
-            while ((read = stream.read()) != -1 && !buffer.toString().endsWith(end))
-                buffer.append((char) read);
+//            if (node instanceof ContainerNode) {
+//                // Read contents from given stream.
+//                //TODO: TOTALLY REWORK!!!
+//                final String end = "</" + tagName + ">";
+//                while ((read = stream.read()) != -1 && !buffer.toString().endsWith(end))
+//                    buffer.append((char) read);
+//
+//                String contents = buffer.toString();
+//                if (contents.endsWith(end)) {
+//                    contents = contents.substring(0, contents.length() - end.length());
+//                    node.setClosed(true);
+//                } else node.setClosed(false);
+//
+//                if (contents.startsWith("<")) {
+//                    InputStream byteStream = new ByteArrayInputStream(contents.getBytes());
+//                    node.setChild(Node.newNode(byteStream));
+//                    buffer.setLength(0);
+//                    while ((read = byteStream.read()) != -1) buffer.append((char) read);
+//                    contents = buffer.toString();
+//                }
+//
+//                node.setContents(contents);
+//            }
 
-            String contents = buffer.toString();
-            if (contents.endsWith(end)) {
-                contents = contents.substring(0, contents.length() - end.length());
-                node.setClosed(true);
-            } else node.setClosed(false);
-
-            if (contents.startsWith("<")) {
-                InputStream byteStream = new ByteArrayInputStream(contents.getBytes());
-                node.setChild(Node.newNode(byteStream));
-                buffer.setLength(0);
-                while ((read = byteStream.read()) != -1) buffer.append((char) read);
-                contents = buffer.toString();
-            }
-
-            node.setContents(contents);
-
-            if (stream.available() > 0) node.setNext(Node.newNode(stream));
+            if (stream.available() > 0) node.setNext(stream);
 
             return node;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new NodeException(e);
         }
-    }
-
-    public static String printNode(Node node) {
-        if (node == null) return null;
-        final StringBuilder builder = new StringBuilder();
-        builder.append("<").append(node.getTagName());
-
-        final Map<String, String> attributes = node.getAttributes();
-        if (attributes != null && !attributes.isEmpty()) {
-            builder.append(" ");
-            for (String key : attributes.keySet()) {
-                final String value = attributes.get(key);
-                builder.append(key);
-                if (value != null) builder.append("=").append(value);
-                builder.append(" ");
-            }
-            builder.setLength(builder.length() - 1);
-        }
-
-        builder.append(">");
-
-        final Node child = node.getChild();
-        if (child != null) builder.append(printNode(child));
-        else {
-            final String contents = node.getContents();
-            if (contents != null) builder.append(contents);
-        }
-
-        if (node.isClosed())
-            builder.append("</").append(node.getTagName()).append(">");
-
-        if (node.getNext() != null)
-            builder.append(Node.printNode(node.getNext()));
-
-        return builder.toString();
     }
 }
--- src/test/java/it/fulminazzo/tagparser/nodes/NodeTest.java
+++ src/test/java/it/fulminazzo/tagparser/nodes/NodeTest.java
@@ -1,65 +1,108 @@
 package it.fulminazzo.tagparser.nodes;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.InputStream;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class NodeTest {
 
-    static Object[][] getNodeTests() {
+    static Object[][] getTagNameTests() {
         return new Object[][]{
-                new Object[]{"body", new LinkedHashMap<>(), "", true, "<body></body>"},
-                new Object[]{"body", new LinkedHashMap<>(), "", false, "<body>"},
-                new Object[]{"body", new LinkedHashMap<>(), "", false, "<body/>"},
-                new Object[]{"body", new LinkedHashMap<String, String>(){{
-                    put("key1", "value1");
-                    put("key2", "'value'\"2\"");
-                    put("key3", "value\"3\"");
-                }}, "", true, "<body key1=value1 key2=\"\\'value\\'\\\"2\\\"\" key3='value\"3\"' ></body>"},
-                new Object[]{"body", new LinkedHashMap<>(), "Contents", true, "<body>Contents</body>"},
-                new Object[]{"body", new LinkedHashMap<>(), "Contents", true, "<body>Contents</body>"},
-                new Object[]{"body", new LinkedHashMap<>(),
-                        new Object[]{"p", new LinkedHashMap<>(), "Contents", true, "<p>Contents</p>"}, true,
-                        "<body><p>Contents</p></body>"},
-                new Object[]{"body", new LinkedHashMap<>(),
-                        new Object[]{"p", new LinkedHashMap<>(), "Contents", true, "<p>Contents</p>"}, true,
-                        "<body><p>Contents</p></body><body><p>Contents</p></body>"},
+                new Object[]{"test", true},
+                new Object[]{"_test", false},
+                new Object[]{"1test", false},
+                new Object[]{"Test", true},
+                new Object[]{"te_st", true},
+                new Object[]{"TE_ST", true},
+                new Object[]{"test_", false},
+                new Object[]{"TESTS_", false},
+                new Object[]{"test-", false},
+                new Object[]{"TEST-", false},
+                new Object[]{"Final_Test99", true},
+                new Object[]{"final_test99", true},
+                new Object[]{"Final_Test", true},
+                new Object[]{"final_test", true}
         };
     }
 
     @ParameterizedTest
-    @MethodSource("getNodeTests")
-    void testNodes(String tagName, LinkedHashMap<String, String> attributes, Object contents, boolean closed, String rawText) {
-        final Node node = Node.newNode(rawText);
-        testNode(node, tagName, closed, attributes, contents);
-        final Node next = node.getNext();
-        if (next != null) testNode(next, tagName, closed, attributes, contents);
+    @MethodSource("getTagNameTests")
+    void testTagNameRegex(String tag, boolean valid) {
+        assertEquals(valid, tag.matches(Node.TAG_NAME_REGEX));
     }
 
-    private void testNode(Node node, String tagName, boolean closed, LinkedHashMap<String, String> attributes, Object contents) {
-        assertEquals(tagName, node.getTagName());
-        assertEquals(closed, node.isClosed());
-        assertEquals(attributes, node.getAttributes());
-        if (contents instanceof String) assertEquals(contents, node.getContents());
-        else {
-            Object[] objects = (Object[]) contents;
-            final Node child = node.getChild();
-            assertEquals(objects[0], child.getTagName());
-            assertEquals(objects[1], child.getAttributes());
-            assertEquals(objects[2], child.getContents());
+    @Nested
+    @DisplayName("Test various methods")
+    class NodeMethods {
+        private Node node;
+
+        @BeforeEach
+        void setUp() {
+            node = new Node("test");
+            node.setNext(new Node("next"));
+        }
+
+        @Test
+        void testSetAttribute() {
+            Map<String, String> map = new LinkedHashMap<>();
+            assertEquals(map, node.getAttributes());
+            map.put("key", "value");
+            node.setAttribute("key", "\"value\"");
+            assertEquals(map, node.getAttributes());
+        }
+
+        @Test
+        void testSetAttributes() {
+            Map<String, String> map = new LinkedHashMap<>();
+            assertEquals(map, node.getAttributes());
+            map.put("key1", "\"value\"");
+            map.put("key2", null);
+            node.setAttributes(map);
+            map.put("key1", "value");
+            assertEquals(map, node.getAttributes());
+        }
+
+        @Test
+        void testToHTML() {
+            setAttributes();
+            final String expected = "<test key1=\"value1\" key2=\"value\\\"2\\\"\" key3=\"value3\" key4/>";
+            assertEquals(expected, node.toHTML());
+        }
+
+        @Test
+        void testToJson() {
+            setAttributes();
+            Gson gson = new GsonBuilder().serializeNulls().create();
+            final String expected = gson.toJson(node);
+            assertEquals(expected, node.toJson());
+        }
+
+        @Test
+        void testToString() {
+            setAttributes();
+            final String expected = "Node {\n" +
+                    "    tagName: \"test\"\n" +
+                    "    attributes: {key1=value1, key2=value\"2\", key3=value3, key4=null}\n" +
+                    "    next: Node(\"next\")\n" +
+                    "}";
+            assertEquals(expected, node.toString());
+        }
+
+        private void setAttributes() {
+            node.setAttribute("key1", "value1");
+            node.setAttribute("key2", "\"value\\\"2\\\"\"");
+            node.setAttribute("key3", "'value3'");
+            node.setAttribute("key4", null);
         }
     }
-
-//    @Test
-//    void testReadHTMLPage() {
-//        InputStream stream = NodeTest.class.getResourceAsStream("/index.html");
-//        assertNotNull(stream);
-//        Node node = Node.newNode(stream);
-//    }
 }
