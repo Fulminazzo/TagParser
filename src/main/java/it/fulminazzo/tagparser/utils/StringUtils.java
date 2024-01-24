e64dcf5dd48edb84658abbf6c8f4666da1e6b19c
Fulminazzo <fulminazzo21@gmail.com>
2024-01-24 02:44:36 +0100

    Added JavaDoc

--- src/main/java/it/fulminazzo/tagparser/utils/StringUtils.java
+++ src/main/java/it/fulminazzo/tagparser/utils/StringUtils.java
@@ -1,8 +1,20 @@
 package it.fulminazzo.tagparser.utils;
 
+/**
+ * The type String utils.
+ */
 public class StringUtils {
     private static final char[] REPLACED_CHARS = new char[]{'"', '\''};
 
+    /**
+     * Unescapes some special characters.
+     * More specifically, single and double quotes are unescaped.
+     * <p>
+     * For example, \" becomes ".
+     *
+     * @param string the string
+     * @return the string
+     */
     public static String removeQuotes(String string) {
         if (string == null) return null;
         if (string.startsWith("'") && string.endsWith("'") ||
